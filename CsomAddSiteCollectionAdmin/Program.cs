059c078e63524b0fc7c50692a1629e6822123186
Sunny Wijeratne <sunny.wijeratne@gmail.com>
2015-11-23 16:55:40 +1000

    initial commit. using csom to designate a user as a site collection admin

---
+++ CsomAddSiteCollectionAdmin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SpoAddSiteCollectionAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = ConfigurationManager.AppSettings["username"];
+            var password = ConfigurationManager.AppSettings["password"];
+            var siteUrl = ConfigurationManager.AppSettings["siteCollecitonUrl"];
+            var newAdminLogonName = ConfigurationManager.AppSettings["newAdminLogonName"];
+
+            var securePassword = new SecureString();
+            foreach (var c in password.ToCharArray()) securePassword.AppendChar(c);
+
+            var clientContext = new ClientContext(siteUrl);
+            clientContext.Credentials = new SharePointOnlineCredentials(username, securePassword);
+
+            var newAdmin = clientContext.Web.EnsureUser(newAdminLogonName);
+            newAdmin.IsSiteAdmin = true;
+            newAdmin.Update();
+            clientContext.Load(newAdmin);
+            clientContext.ExecuteQuery();
+        }
+    }
+}
